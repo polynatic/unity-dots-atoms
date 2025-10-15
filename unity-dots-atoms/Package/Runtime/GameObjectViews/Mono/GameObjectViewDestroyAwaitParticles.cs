1470c3e64178eb0cb3f7ba3b44d03281b9875b63
Thomas Loreit <tl@polynatic.com>
2025-10-15 18:26:49 +0200

    Add component for finishing particles before destroying GameObjectView

---
+++ unity-dots-atoms/Package/Runtime/GameObjectViews/Mono/GameObjectViewDestroyAwaitParticles.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using DotsAtoms.GameObjectViews.Interfaces;
+using Unity.Entities;
+using UnityEngine;
+using static Cysharp.Threading.Tasks.UniTask;
+
+namespace DotsAtoms.GameObjectViews.Mono
+{
+    public class GameObjectViewDestroyAwaitParticles : MonoBehaviour, IGameObjectView
+    {
+        private ParticleSystem[] ParticleSystems;
+
+        private void Awake()
+        {
+            ParticleSystems = GetComponentsInChildren<ParticleSystem>();
+            var subEmitters = new HashSet<ParticleSystem>();
+
+            // filter all particle systems that are sub emitters
+            foreach (var system in ParticleSystems) {
+                var subEmittersModule = system.subEmitters;
+
+                if (!subEmittersModule.enabled) continue;
+
+                for (var i = 0; i < subEmittersModule.subEmittersCount; i++) {
+                    var subEmitter = subEmittersModule.GetSubEmitterSystem(i);
+                    if (!subEmitter) continue;
+
+                    subEmitters.Add(subEmitter);
+                }
+            }
+
+            if (subEmitters.Count == 0) return;
+
+            ParticleSystems = ParticleSystems.Except(subEmitters).ToArray();
+        }
+
+        public void OnViewAttached(EntityManager entityManager, in Entity entity, EntityCommandBuffer commands) { }
+        public void OnViewDetached(EntityManager entityManager, in Entity entity, EntityCommandBuffer commands) { }
+
+        public async UniTask OnViewDestroy()
+        {
+            var tasks = new List<UniTask>(ParticleSystems.Length);
+
+            foreach (var system in ParticleSystems) {
+                system.Stop();
+
+                if (system.IsAlive(withChildren: true)) {
+                    tasks.Add(WaitForParticleSystemToFinish(system));
+                }
+            }
+
+            if (tasks.Count > 0) {
+                await tasks;
+            }
+        }
+
+        private async UniTask WaitForParticleSystemToFinish(ParticleSystem system)
+        {
+            while (system.IsAlive(withChildren: true)) {
+                await NextFrame(cancellationToken: destroyCancellationToken);
+            }
+        }
+    }
+}
